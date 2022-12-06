1d24fa63c29b4200a4d0ce9df74cbeaca0e08d85
monielle <monielle@gmail.com>
2022-12-06 18:25:27 -0300

    Inserindo arquivo import sql e finalizando as camadas

--- src/main/java/com/example/estudo/demo/controllers/UserController.java
+++ src/main/java/com/example/estudo/demo/controllers/UserController.java
@@ -1,4 +1,21 @@
 package com.example.estudo.demo.controllers;
 
-public class UserController git checkout{
+import com.example.estudo.demo.dto.UserDTO;
+import com.example.estudo.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = "/{id}")
+    public UserDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
 }
--- src/main/java/com/example/estudo/demo/dto/UserDTO.java
+++ src/main/java/com/example/estudo/demo/dto/UserDTO.java
@@ -21,5 +21,19 @@ public class UserDTO {
         name = user.getName();
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
